cc71515b8dd2a6bace4fb25f89205fb77e2d5c69
AleRiccardi <alericcardi1996@gmail.com>
2018-01-10 17:12:01 +0100

    Adapted style for user page and fixed header problems

--- listify-child/header.php
+++ listify-child/header.php
@@ -73,43 +73,6 @@ $current_user = wp_get_current_user();
 				<?php endif; ?>
 			</div>
 		</div>
-
-		<!--<nav id="site-navigation" class="main-navigation<?php if ( is_front_page() ) : ?> main-navigation--<?php echo get_theme_mod( 'home-header-style', 'default' ); ?><?php endif; ?>">
-			<div class="container">
-				<a href="#" class="navigation-bar-toggle">
-					<i class="ion-navicon-round"></i>
-					<span class="mobile-nav-menu-label"><?php echo listify_get_theme_menu_name( 'primary' ); ?></span>
-				</a>
-
-				<div class="navigation-bar-wrapper">
-					<?php
-						wp_nav_menu( array(
-							'theme_location' => 'primary',
-							'container_class' => 'primary nav-menu',
-							'menu_class' => 'primary nav-menu',
-						) );
-
-						if ( listify_theme_mod( 'nav-secondary', true ) ) {
-							wp_nav_menu( array(
-								'theme_location' => 'secondary',
-								'container_class' => 'secondary nav-menu',
-								'menu_class' => 'secondary nav-menu',
-							) );
-						}
-					?>
-				</div>
-
-				<?php if ( 'none' !== get_theme_mod( 'nav-search', 'left' ) ) : ?>
-					<a href="#search-navigation" data-toggle="#search-navigation" class="ion-search search-overlay-toggle"></a>
-
-					<div id="search-navigation" class="search-overlay">
-						<?php locate_template( array( 'searchform-header.php', 'searchform.php' ), true, false ); ?>
-
-						<a href="#search-navigation" data-toggle="#search-navigation" class="ion-close search-overlay-toggle"></a>
-					</div>
-				<?php endif; ?>
-			</div>
-		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<?php do_action( 'listify_content_before' ); ?>
