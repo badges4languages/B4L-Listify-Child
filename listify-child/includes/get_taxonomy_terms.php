384695cd9651019cf12274f3a4d97528c769c614
leocharlier <leo.charlier.music@gmail.com>
2018-07-10 17:26:12 +0200

    Field of education correction

--- listify-child/includes/get_taxonomy_terms.php
+++ listify-child/includes/get_taxonomy_terms.php
@@ -8,7 +8,7 @@
 function get_CPT_terms($postID, $term){
 	$terms_list = wp_get_post_terms($postID, $term);
 	if( empty( $terms_list ) ) :
-		return 'Not Specified';
+		return 'All of them';
 	else :
 		$output = '';
 		$i = 0;
