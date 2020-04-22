83b482d4909b1db952124f869c72f055904a3b65
Colomet <colomet@hotmail.com>
2020-04-22 13:01:03 +0200

    unsync update github

--- functions.php
+++ functions.php
@@ -7,21 +7,6 @@ function listify_child_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'listify_child_styles', 999 );
 
-/* ////////////////////////////////
-   /     Plugin Update Checker    /
-   //////////////////////////////// */
-
-/*
-* Auto update from github
-*
-* @since 1.0.0
-*/
-require 'vendor/plugin-update-checker/plugin-update-checker.php';
-$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-   'https://github.com/my-language-skills/open-badges-framework-listify-child/',
-   __FILE__,
-   'open-badges-framework-listify-child'
-);
 
 //Include all the files for shortcodes, menus, admin panel, taxonomies and files size option
 require_once( "includes/include_script.php" );
