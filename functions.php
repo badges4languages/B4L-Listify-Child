e4a7a91d3f448c91646d8a1c0ecfd58784ca9950
Colomet <colomet@hotmail.com>
2020-04-22 13:03:39 +0200

    RCP Templates

--- functions.php
+++ functions.php
@@ -15,3 +15,18 @@ require_once( "includes/custom_menu.php" );
 require_once( "includes/admin.php" );
 require_once( "includes/get_taxonomy_terms.php" );
 require_once( "includes/limit_upload_size.php" );
+
+
+/**
+* Function for adding a new RCP Template (mails).
+*
+* SINCE v0.1
+*/
+
+function ag_rcp_email_templates( $templates ) {
+    $templates['badges4languages'] = __( 'Badges4Languages Template' );
+
+    return $templates;
+}
+
+add_filter( 'rcp_email_templates', 'ag_rcp_email_templates' );
